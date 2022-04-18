1ca3bcea1718a85a17e0608ab7a1ff00db3b73c3
myOmikron <git@omikron.dev>
2022-04-18 05:52:38 +0200

    Added emit lexer command

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -2,20 +2,33 @@ use std::path::Path;
 
 mod lexer;
 
-pub fn run_compiler(path: &str, out_dir: &str) {
-    // Check if file exists
+fn check_file_existence(path: &str) -> bool {
     let file = Path::new(path);
     if !file.exists() {
         println!("{} does not exist", path);
-        return;
+        return false;
     }
     if !file.is_file() {
         println!("{} is not a file", path);
+        return false;
+    }
+    return true;
+}
+
+pub fn run_compiler(path: &str, out_dir: &str, emit_lexer: bool) {
+    if !check_file_existence(path) {
         return;
     }
-
     let tokenized = lexer::lex(path);
-    for token in tokenized {
-        println!("{}", token)
+    if emit_lexer {
+        for token in tokenized {
+            println!("{}", token)
+        }
+    }
+}
+
+pub fn run_compiler_tmp(path: &str) {
+    if !check_file_existence(path) {
+        return;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,12 @@ enum Commands {
         #[clap(short = 'o', long = "out")]
         #[clap(default_value_t=String::from("./bin/"))]
         out_dir: String,
+        #[clap(long = "emit-lexer")]
+        #[clap(takes_value = false)]
+        emit_lexer: bool,
     },
     #[clap(about = "Compile and run")]
-    Run { file: Option<String> },
+    Run { path: String },
 }
 
 #[derive(Parser)]
@@ -28,10 +31,14 @@ fn main() {
     let cli: CLI = CLI::parse();
 
     match cli.command {
-        Some(Commands::Build { path, out_dir }) => {
-            compiler::run_compiler(&path, &out_dir);
+        Some(Commands::Build {
+            path,
+            out_dir,
+            emit_lexer,
+        }) => {
+            compiler::run_compiler(&path, &out_dir, emit_lexer);
         }
-        Some(Commands::Run { file }) => {}
+        Some(Commands::Run { path }) => compiler::run_compiler_tmp(&path),
         _ => {}
     }
 }
